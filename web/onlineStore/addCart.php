5d04057f57b47e9c28eddce290f1997c0298e515
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-02 22:39:50 -0600

    Started PHP

--- web/onlineStore/addCart.php
+++ web/onlineStore/addCart.php
@@ -1,3 +1,19 @@
 <?php
-echo $_GET["sku"];
+
+session_start();
+
+try 
+{
+  if(isset($_GET["sku"]))
+  {
+    array_push($_SESSION["sku"], $_GET["sku"]);
+  }
+
+  echo "All data =  " . print_r($_SESSION["sku"]);
+}
+catch (Exception $e)
+{
+  echo 'Caught exception: ', $e->getMessage();
+}
+
 ?>
