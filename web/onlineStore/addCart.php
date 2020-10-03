b2ad48d2c4c2903e8deea8933ebc9eb46012efce
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-02 22:48:47 -0600

    Started PHP

--- web/onlineStore/addCart.php
+++ web/onlineStore/addCart.php
@@ -7,7 +7,7 @@ try
   if(isset($_GET["sku"]))
   {
     echo "adding to array...";
-    array_push($_SESSION["sku"], $_GET["sku"]);
+    array_push($_SESSION["sku"], "Blue");
   }
 
   echo print_r($_SESSION["sku"]);
