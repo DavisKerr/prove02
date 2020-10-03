34fb265369542a031101db389f398102cfcf7d63
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-02 22:29:47 -0600

    Started PHP

--- web/onlineStore/addCart.php
+++ web/onlineStore/addCart.php
@@ -2,8 +2,7 @@
 echo "running";
 if isset($_GET["sku"])
 {
-  array_push($_SESSION["sku"], $_GET["sku"]);
-  echo "Success in loading " . print_r($_SESSION["sku"]) . " into the server!";
+  echo "It's set";
 }
 
 ?>
