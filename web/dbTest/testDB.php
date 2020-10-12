f34f541b2f1553a0689c1dccffcaec39b574a0bc
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-12 13:29:15 -0600

    Added a test query for the database

--- web/dbTest/testDB.php
+++ web/dbTest/testDB.php
@@ -29,9 +29,7 @@ $id = 1;
 $name = 'john';
 
 $stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
-$stmt->bindValue(':id', $id, PDO::PARAM_INT);
-$stmt->bindValue(':name', $name, PDO::PARAM_STR);
-$stmt->execute();
+$stmt->execute(array(':name' => $name, ':id' => $id));
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo print_r($rows);
