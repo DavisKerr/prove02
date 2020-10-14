cc97c611fb00bdeb1bdb6e4b727729f562ee6acd
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-14 09:00:47 -0600

    Added the battle boats project.

--- web/battleBoats/login.php
+++ web/battleBoats/login.php
@@ -7,7 +7,7 @@
   {
     $stmt = $database->prepare('SELECT username, password FROM public.user WHERE username=:username AND password=:password');
     $stmt->execute(array(':username' => $v_username, ':password' => $v_passwrd));
-    $db_username = $db_password = ''
+    $db_username = $db_password = '';
     foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $row)
     {
       $db_username = $row["username"];
