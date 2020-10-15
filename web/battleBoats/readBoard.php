56fe5ba75d4b7d96a583f303cd8ef7e0fdc26f07
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-15 11:07:08 -0600

    Getting less frustrated.

--- web/battleBoats/readBoard.php
+++ web/battleBoats/readBoard.php
@@ -1,27 +1,3 @@
-<?php 
-/*$board = '
-* * * * * * * * * * /
-* * * * * * * * * * / 
-* * * * * * * * * * / 
-* * O * * X * * * * / 
-* * * * * V * * * * /
-* * O * * V * * * * / 
-* * * * * * * * * * / 
-* V V V V V * * * * /
-* * * * * * * * * * / 
-* * * * * * * * * * /
-';
-$array = str_split($board);*/
-echo "<table>\n";
-echo "<tr>\n";
-for($i = 1; $i <= 10, $i++ )
-{
-  echo "<th>" . $i . "/<th>\n";
-}
-
-echo "</table>\n"
-?>
-
 <!DOCTYPE html>
 <html lang="en-US">
 
