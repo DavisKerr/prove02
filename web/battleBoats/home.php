47a5b64b4ca25e422b408a34fe7ee5111a990330
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-14 20:38:54 -0600

    Added the battle boats project.

--- web/battleBoats/home.php
+++ web/battleBoats/home.php
@@ -122,8 +122,8 @@
               foreach($db_arr as $row)
               {
                 echo "<tr>\n";
-                /*echo "<td>" .  $row["game_name"] . "</td>\n";
-                echo "<td>" .  $row["date_created"]; . "</td>\n";
+                echo "<td>" .  $row["game_name"] . "</td>\n";
+                /*echo "<td>" .  $row["date_created"]; . "</td>\n";
                 echo "<td>" .  $row["game_owner"]; . "</td>\n";*/
                 echo "</tr>\n";
               }
