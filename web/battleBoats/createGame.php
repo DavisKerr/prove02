a0aa88b4a24acfc591499cb937531a9667e5f329
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-20 19:02:45 -0600

    Created stub functions for registering.

--- web/battleBoats/createGame.php
+++ web/battleBoats/createGame.php
@@ -84,14 +84,15 @@ function validateCode($data)
     echo "ERROR: " . $e->getMessage() . "\n";
   } 
 }
-/*
+
+
 function test_input($data) 
 {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
-}*/
+}
 
 function insertRecord($data, $db)
 {
