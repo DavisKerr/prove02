8bbda3f7538d0d42953c45057696c79923b2af16
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-20 19:10:28 -0600

    Created stub functions for registering.

--- web/battleBoats/createGame.php
+++ web/battleBoats/createGame.php
@@ -6,6 +6,7 @@ function isNewGame($db)
   {
     if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["gameName"]))
     {
+      echo "YEET!";
       $dataArr = array("gameName"=>'', "type"=>'PUBLIC', "code"=>'', "gameNameErr"=>'', "codeErr"=>'', "isValid"=>TRUE);
       $dataArr = validateForm($dataArr);
       if($dataArr["isValid"])
