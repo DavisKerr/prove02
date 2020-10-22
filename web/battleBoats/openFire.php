9b6dbdbd900b38fe28e24b6ac575eb322678b63c
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-21 23:41:37 -0600

    Testing the completed join game functions

--- web/battleBoats/openFire.php
+++ web/battleBoats/openFire.php
@@ -3,15 +3,12 @@
 
 function isFiring($db, $enemyData)
 {
-  print_r($enemyData);
-  if($_SERVER["REQUEST_METHOd"] == "POST")
+  if($_SERVER["REQUEST_METHOD"] == "POST")
   {
-    echo $_POST["x-coord"] . ", " . $_POST["y-coord"] . "<br>";
     if(isset($_POST["x-coord"]))
     {
       if(isset($_POST["y-coord"]))
       {
-        echo "Passed.";
         $board = fire($_POST["x-coord"], $_POST["y-coord"], $enemyData["board"]);
         if($board == "ERROR")
         {
