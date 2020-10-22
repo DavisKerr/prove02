850f0c56f15ee7876673ff6b513367a898a69b34
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-22 00:08:40 -0600

    Testing the completed join game functions

--- web/battleBoats/openFire.php
+++ web/battleBoats/openFire.php
@@ -64,7 +64,7 @@ function updateBoard($db, $board, $which)
 {
   $query = "UPDATE public.game SET :which = :newBoard WHERE id = :game_id";
   $statement = $db->prepare($query);
-  //$statement->execute(array(':game_id'=>$_SESSION["current_game_id"], ":newBoard"=>$board, ":which"=>$which));
+  $statement->execute(array(':game_id'=>$_SESSION["current_game_id"], ":newBoard"=>$board, ":which"=>$which));
 }
 
 ?>
