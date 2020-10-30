56ba8911548b64cd24b3b8e0475cdef06b46d9dc
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-30 13:40:38 -0600

    Testing the completed join game functions

--- web/battleBoats/database/queryUsers.php
+++ web/battleBoats/database/queryUsers.php
@@ -2,18 +2,18 @@
 
   function queryUsers($username, $password, $database)
   {
-    $db_data = array("db_username"=>'', "db_password"=>'', "db_id"=>'', "db_err"=>'');
+    $db_data = array(/*"db_username"=>'', "db_password"=>'', "db_id"=>'',*/ "db_err"=>'');
     try
     {
       $stmt = $database->prepare('SELECT id, username, password FROM public.user WHERE username=:username AND password=:password');
       $stmt->execute(array(':username' => $username, ':password' => $password));
-      
-      foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $row)
+      $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      /*foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $row)
       {
         $db_data["db_username"] = $row["username"];
         $db_data["db_password"] = $row["password"];
         $db_data["db_id"] = $row["id"];
-      }
+      }*/
       return $db_data;
     }
     catch(PDOException $e)
