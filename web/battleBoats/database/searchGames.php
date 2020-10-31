c868ca94d4dfc10c9a021960e2aaa624915ba691
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-31 13:50:56 -0600

    Started working on searching games.

--- web/battleBoats/database/searchGames.php
+++ web/battleBoats/database/searchGames.php
@@ -6,65 +6,60 @@
     {
       $query = '';
 
-      if($type == 1) // 1 means it is an active game.
-      {
-        $query = " 
-        SELECT g.id, g.game_name, g.date_created, g.game_type, u.display_name AS Player1, u2.display_name AS player2
-        FROM public.game AS g
-        JOIN public.user AS u 
-        ON g.game_owner = u.id
-        JOIN public.user AS u2
-        ON g.opponent = u2.id
-        WHERE (g.opponent =:player_id
-        or g.game_owner =:player_id)
-        AND g.is_active = 1
-        AND LOWER(g.game_name) like LOWER(:search)
-        ORDER BY g.date_created";
 
-        $stmt = $database->prepare($query);
-        $stmt->execute(array(':player_id'=>$_SESSION['user_id'], ':search'=>$search));
-        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $tableData["active"] = $rows;
-      }
+      $query = " 
+      SELECT g.id, g.game_name, g.date_created, g.game_type, u.display_name AS Player1, u2.display_name AS player2
+      FROM public.game AS g
+      JOIN public.user AS u 
+      ON g.game_owner = u.id
+      JOIN public.user AS u2
+      ON g.opponent = u2.id
+      WHERE (g.opponent =:player_id
+      or g.game_owner =:player_id)
+      AND g.is_active = 1
+      AND LOWER(g.game_name) like LOWER(:search)
+      ORDER BY g.date_created";
 
-      if($type == 2) // 2 means it is a public game that is joinable.
-      {
-        $query = " 
-        SELECT g.id, g.game_name, g.date_created, u.display_name AS Player1 
-        FROM public.game AS g 
-        JOIN public.user AS u 
-        ON g.game_owner = u.id 
-        WHERE g.is_active = 0 
-        AND g.game_type LIKE 'PUBLIC' 
-        AND LOWER(g.game_name) LIKE LOWER(:search)
-        AND g.game_owner <> :user_id
-        ORDER BY g.date_created";
+      $stmt = $database->prepare($query);
+      $stmt->execute(array(':player_id'=>$_SESSION['user_id'], ':search'=>$search));
+      $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      $tableData["active"] = $rows;
 
-        $stmt = $database->prepare($query);
-        $stmt->execute(array(':user_id'=>$_SESSION['user_id'], ':search'=>$search));
-        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $tableData["public"] = $rows;
-      }
 
-      if($type == 3) // 3 means it is a private game that is joinable.
-      {
-        $query = "
-        SELECT g.id, g.game_name, g.date_created, u.display_name As Player1
-        FROM public.game AS g 
-        JOIN public.user AS u 
-        ON g.game_owner = u.id 
-        WHERE g.is_active = 0 
-        AND g.game_type LIKE 'PRIVATE' 
-        AND LOWER(g.game_code) LIKE LOWER(:search)
-        AND g.game_owner <> :user_id
-        ORDER BY g.date_created
-        ";
+      $query = " 
+      SELECT g.id, g.game_name, g.date_created, u.display_name AS Player1 
+      FROM public.game AS g 
+      JOIN public.user AS u 
+      ON g.game_owner = u.id 
+      WHERE g.is_active = 0 
+      AND g.game_type LIKE 'PUBLIC' 
+      AND LOWER(g.game_name) LIKE LOWER(:search)
+      AND g.game_owner <> :user_id
+      ORDER BY g.date_created";
 
-        $stmt = $database->prepare($query);
-        $stmt->execute(array(':search'=>$search, ':user_id'=>$_SESSION['user_id']));
-        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $tableData["private"] = $rows;
-      }
+      $stmt = $database->prepare($query);
+      $stmt->execute(array(':user_id'=>$_SESSION['user_id'], ':search'=>$search));
+      $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      $tableData["public"] = $rows;
+
+
+
+      $query = "
+      SELECT g.id, g.game_name, g.date_created, u.display_name As Player1
+      FROM public.game AS g 
+      JOIN public.user AS u 
+      ON g.game_owner = u.id 
+      WHERE g.is_active = 0 
+      AND g.game_type LIKE 'PRIVATE' 
+      AND LOWER(g.game_code) LIKE LOWER(:search)
+      AND g.game_owner <> :user_id
+      ORDER BY g.date_created
+      ";
+
+      $stmt = $database->prepare($query);
+      $stmt->execute(array(':search'=>$search, ':user_id'=>$_SESSION['user_id']));
+      $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      $tableData["private"] = $rows;
 
       /*
       $query = "
