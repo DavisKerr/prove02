00498137f4743b6c5d2795cf989d18c1626dc9cc
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-31 14:55:08 -0600

    Started working on joining games.

--- web/battleBoats/Scripts/joinGame.js
+++ web/battleBoats/Scripts/joinGame.js
@@ -20,3 +20,25 @@ $(document).ready(function(){
       }); 
   });
 });
+
+function joinGame(button)
+{
+  var game_id = this.value;
+  alert(game_id);
+  $.post("../Games/joinGame.php",
+  {
+    id: game_id
+  },
+  function(data, status)
+  { 
+    if(status == 'success')
+    {
+      //window.location.replace("./landing.php");
+    }
+    else
+    {
+      alert("Oops! Something happened!");
+    }
+    
+  }); 
+}
