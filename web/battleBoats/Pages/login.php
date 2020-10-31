2b9de37e70790cb750d334edf6d5ff874df6418b
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-30 21:01:40 -0600

    Testing the completed join game functions

--- web/battleBoats/Pages/login.php
+++ web/battleBoats/Pages/login.php
@@ -6,9 +6,12 @@
 
   if($_SERVER["REQUEST_METHOD"] == "POST")
   {
-    $_SESSION["loggedIn"] = true;
-    header("Location: ./home.php");
-    exit();
+    if($_SESSION["loggedIn"] == true)
+    {
+      header("Location: ./home.php");
+      exit();
+    }
+    //$_SESSION["loggedIn"] = true;
   }
 ?>
 
