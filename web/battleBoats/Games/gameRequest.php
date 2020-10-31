dd381caeae4902dc802307ac4a0fc9ed49b68ea8
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-31 16:04:18 -0600

    Started working on joining games.

--- web/battleBoats/Games/gameRequest.php
+++ web/battleBoats/Games/gameRequest.php
@@ -5,7 +5,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
   if(isset($_POST["playGame"]))
   {
     $_SESSION["current_game_id"] = $_POST["playGame"]; 
-    header("location: ../pages/play.php");
+    header("location: ../Pages/play.php");
     exit;
   }
 }
