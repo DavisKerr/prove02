162493cb3cefa03696b4bbb863c3c6e1c34cd7f0
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-31 20:40:42 -0600

    Started working on the actually play page

--- web/battleBoats/database/updateBoard.php
+++ web/battleBoats/database/updateBoard.php
@@ -3,9 +3,9 @@
   {
     try
     {
-      /*$query = "UPDATE public.board SET grid = :newBoard WHERE game_id = :game_id AND board_owner <> :user_id";
+      $query = "UPDATE public.board SET grid = :newBoard WHERE game_id = :game_id AND board_owner <> :user_id";
       $statement = $db->prepare($query);
-      $statement->execute(array(':game_id'=>$_SESSION["current_game_id"], ":newBoard"=>$board, ':user_id'=>$_SESSION['user_id']));*/
+      /*$statement->execute(array(':game_id'=>$_SESSION["current_game_id"], ":newBoard"=>$board, ':user_id'=>$_SESSION['user_id']));*/
     }
     catch(Exception $e)
     {
