f0309f14a2e4472475eaa8ad2df4380a5de2ac76
Davis Kerr <davis.kerr1997@gmail.com>
2020-10-31 19:27:46 -0600

    Started working on the actually play page

--- web/battleBoats/Scripts/turns.js
+++ web/battleBoats/Scripts/turns.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
   checkTurn();
+
+  $('#fireBtn').click(function(){
+    var x = document.getElementById("x-coord").value;
+    var y = document.getElementById("y-coord").value;
+    alert(x);
+      $.post("../gameLogic/fire.php",
+      {
+      x: x,
+      y: y
+      },
+      function(data, status)
+      { 
+        processFire(data);
+      }); 
+  });
+
 });
 
 function checkTurn()
@@ -33,4 +49,22 @@ function checkTurn()
 function changeForm(data)
 {
   document.getElementById('moveForm').innerHTML = data.form;
+}
+
+function processFire(data)
+{
+  if(data.serverError != '')
+  {
+    alert(data.serverError);
+  }
+  else if(data.isValid == false)
+  {
+    document.getElementById("fireErr").innerHTML = 'Already tried there!';
+  }
+  else
+  { 
+    checkGameEnd();
+    checkTurn();
+  }
+  
 }
